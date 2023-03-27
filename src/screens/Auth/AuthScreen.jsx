864ed6768888c9febcc16e6253321fa8339d14a1
Nicolas Temprano <nicomxd22@gmail.com>
2023-03-26 23:58:04 -0300

    last modifications

--- src/screens/Auth/AuthScreen.jsx
+++ src/screens/Auth/AuthScreen.jsx
@@ -8,7 +8,7 @@ const AuthScreen = ({ navigation }) => {
     const authScreenText = colorScheme === "light" ? styles.authScreenText : styles.authScreenTextDark;
 
     return (
-        <ImageBackground style={styles.authScreenBackground} source={require('../../../assets/images/AuthBackground.jpg')} resizeMode="cover">
+        <ImageBackground style={styles.authScreenBackground} source={{uri: "https://media.istockphoto.com/id/1309279439/photo/making-payments-the-simple-way.jpg?b=1&s=170667a&w=0&k=20&c=G2sJ4ZZanF7rfROgk4ie6ZTT6s5Q6bouWKFEu_0OcxU="}} resizeMode="cover">
             <KeyboardAvoidingView behavior='height' style={styles.authScreen}>
                 <View style={styles.authScreenImageContainer}>
                     <Image source={require("../../../assets/images/logo/logo-low-resolution-no-background.png")} style={styles.authScreenImage}/>
